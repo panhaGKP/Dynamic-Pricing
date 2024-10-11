c7242af4e543bb4969a2c65b9c6c122687e65e20
Panha VUTHY <panha.vuthy@solution-bi.com>
2024-10-11 14:47:46 +0700

    Restructure the flow of Product Training for XGBoost

---
+++ pages/product_training.py
@@ -0,0 +1,395 @@
+import streamlit as st
+import os
+import pandas as pd
+import numpy as np
+import cachetools
+import json
+
+from snowflake.snowpark import Session
+import snowflake.snowpark.functions as F
+import snowflake.snowpark.types as T
+
+from datetime import datetime
+from dotenv import load_dotenv
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from xgboost import XGBRegressor # type: ignore
+from joblib import dump
+from snowflake.snowpark.types import StructType, StructField, IntegerType, FloatType, StringType
+from joblib import dump
+
+# from snowflake.snowpark.context import get_active_session
+
+
+st.set_page_config(page_title="Product Training", page_icon="😊")
+# Title for the app
+st.title('Product Training Portal')
+
+
+# ============ function Definitions part ==============
+# Refresh Snowflake session after 60 minites
+@st.cache_resource(ttl=3600)
+def get_active_session():
+    #get account credentials from
+    load_dotenv()
+    connection_parameters = {
+        "user": os.getenv("SNOWFLAKE_USER"),
+        "password": os.getenv("SNOWFLAKE_PASSWORD"),
+        "account": os.getenv("SNOWFLAKE_ACCOUNT"),
+        "warehouse": os.getenv("SNOWFLAKE_WAREHOUSE"),
+        "database": os.getenv("SNOWFLAKE_DATABASE"),
+        "schema": os.getenv("SNOWFLAKE_SCHEMA"),
+        "role": os.getenv("SNOWFLAKE_ROLE"),
+        "query_tag": "streamlit-app product-training"
+    }
+    return Session.builder.configs(connection_parameters).create()
+
+def convert_list_to_df(data, columns):
+    return pd.DataFrame(data, columns=columns)
+
+def treat_outliers(df, column, outlier_percentages):
+    Q1 = df[column].quantile(0.25)
+    Q3 = df[column].quantile(0.75)
+    IQR = Q3 - Q1
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+    # if any of the numerical columns have outliers that under 5%, we can keep them, otherwise we treat theme with mean imputation
+    if outlier_percentages[column] > 5:
+        df[column] = np.where(df[column] < lower_bound, df[column].mean(), df[column])
+        df[column] = np.where(df[column] > upper_bound, df[column].mean(), df[column])
+    return df
+
+def create_table_if_not_exists(table_name):
+    create_table_query = f"""
+    CREATE TABLE IF NOT EXISTS {table_name} (
+        DATE_ORDINAL INT,
+        UNITS_SOLD INT,
+        CUSTOMER_SEGMENT INT,
+        AVG_DISCOUNT_RATE FLOAT,
+        PRODUCT_SKU STRING
+    );
+    """
+    session.sql(create_table_query).collect()  # Execute the query
+
+def insert_pandas_dataframe_to_snowflake(session, pandas_df, table_name, schema):
+    # Convert pandas DataFrame to Snowpark DataFrame
+    snowpark_df = session.create_dataframe(pandas_df, schema=schema)
+    product_sku = pandas_df['PRODUCT_SKU'][0]
+    # Write the Snowpark DataFrame into Snowflake table
+    session.sql(f"DELETE FROM {table_name} WHERE PRODUCT_SKU = '{product_sku}'").collect()
+    snowpark_df.write.mode("append").save_as_table(f"{table_name}")
+    return snowpark_df
+
+def get_modile_file_name_from_stage(session, stage_name):
+    files = session.sql(f"LIST @{stage_name}").collect()
+    model_files = [file['name'] for file in files if file['name'].endswith('.sav')]
+
+    #insert file name without prefix dynamic_pricing_model_stage/
+    model_files = [file.replace(f"{stage_name.lower()}/", "") for file in model_files]
+    return model_files # list data type
+
+@cachetools.cached(cache={})
+def load_model(filename):
+
+    # Import packages
+    import sys
+    import os
+    import joblib
+
+    # Get the import directory where the model file is stored
+    import_dir = sys._xoptions.get("snowflake_import_directory")
+
+    # Get the import directory where the model file is stored
+    if import_dir:
+        with open(os.path.join(import_dir, filename), "rb") as f:
+            model = joblib.load(f)
+            return model
+
+# Function to predict the average discount rate
+def avg_discount_rate_prediction(date_ordinal: int, units_sold: int, customer_segment: int, model_name: str) -> float:
+    import pandas as pd
+    # Create a DataFrame from the input values (this assumes feature_cols)
+    feature_cols = ['DATE_ORDINAL', 'UNITS_SOLD', 'CUSTOMER_SEGMENT']
+    X = pd.DataFrame([[date_ordinal, units_sold, customer_segment]], columns=feature_cols)
+
+    # Load the model using the model name
+    model = load_model(model_name)
+
+    # Get predictions
+    predictions = model.predict(X)
+
+    # Return the predicted value (assuming a regression model returning a single value)
+    return predictions[0]
+
+def insert_trained_result_to_snowflake(session, product_sku, df_model, model_file_name, evaluation_metrics, hyperparameters):
+    product_trained = session.sql(f"""
+        SELECT * FROM LU_PRD_PRODUCT_TRAINED WHERE PRODUCT_SKU = '{product_sku}'
+    """).collect()
+
+    if len(product_trained) > 0:
+        st.write("Product used to be trained, and being updated")
+        session.sql(f"""
+            UPDATE LU_PRD_PRODUCT_TRAINED 
+            SET 
+                PRODUCT_SKU = '{product_sku}',
+                MIN_VOLUMN_SOLD = {df_model['UNITS_SOLD'].min()},
+                MAX_VOLUMN_SOLD = {df_model['UNITS_SOLD'].max()},
+                DATE_MODIFIED = CURRENT_TIMESTAMP(),
+                MODEL_PATH = '{model_file_name}',
+                MODEL_NAME = 'xgboost',
+                EVALUATION = PARSE_JSON('{json.dumps(evaluation_metrics)}'),
+                HYPER_PARAMS = PARSE_JSON('{json.dumps(hyperparameters)}')
+            WHERE PRODUCT_SKU = '{product_sku}'
+        """).collect()
+
+    else:
+        print("Product is new to be trained")
+        session.sql(f"""
+            INSERT INTO LU_PRD_PRODUCT_TRAINED (PRODUCT_SKU, MIN_VOLUMN_SOLD, MAX_VOLUMN_SOLD, DATE_CREATED, DATE_MODIFIED, MODEL_PATH, MODEL_NAME, EVALUATION, HYPER_PARAMS)
+            SELECT '{product_sku}', {df_model['UNITS_SOLD'].min()}, {df_model['UNITS_SOLD'].max()}, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), '{model_file_name}', 'xgboost', PARSE_JSON('{json.dumps(evaluation_metrics)}'), PARSE_JSON('{json.dumps(hyperparameters)}')
+        """).collect()
+
+# ============ End of function Definitions =========
+# ============ Main Code ==============
+session = get_active_session()
+
+gs_psku_list = session.sql("""
+    SELECT
+        LU_PRD_PRODUCT_SKU.PRODUCT_SKU AS "PRD_PRODUCT_SKU",
+        SUM(FACT_ORDER_LINE_ITEM_BASE.ORDERED_QUANTITY * FACT_ORDER_LINE_ITEM_BASE.RETAIL_PRICE) AS "GS"
+    FROM
+        FACT_ORDER_LINE_ITEM_BASE
+    JOIN 
+        LU_PRD_PRODUCT
+    ON
+        LU_PRD_PRODUCT.PRODUCT_CODE = FACT_ORDER_LINE_ITEM_BASE.PRODUCT_CODE
+    JOIN 
+        LU_PRD_PRODUCT_SKU 
+    ON
+        LU_PRD_PRODUCT_SKU.PRODUCT_SKU = LU_PRD_PRODUCT.PRODUCT_SKU
+    WHERE
+        (YEAR(FACT_ORDER_LINE_ITEM_BASE.ORDER_DATE) BETWEEN 2021 AND 2023)
+        AND LU_PRD_PRODUCT_SKU.PRODUCT_SKU NOT IN (SELECT PRODUCT_SKU FROM LU_PRD_PRODUCT_TRAINED)
+        
+    GROUP BY PRD_PRODUCT_SKU
+    ORDER BY GS DESC;
+""").collect()
+
+gs_psku_df = convert_list_to_df(gs_psku_list, ["PRODUCT_SKU", "GROSS_SALES"])
+
+products_trained = gs_psku_df["PRODUCT_SKU"].tolist()
+#products = ['B15_S1', 'B13_S1', 'B31_S32', 'B44_S4', 'B15_S47']
+# Product input - String list
+product_sku_trained_selected = st.selectbox(
+    'Choose a Product',
+    products_trained  # Add or change the product names as needed
+)
+
+weekly_gross_sales_by_product = session.sql(f"""
+    SELECT 
+        DATE_TRUNC('week', FACT_ORDER_LINE_ITEM_BASE.ORDER_DATE) AS "WEEK",
+        SUM(LU_PRD_PRODUCT_SKU.PRODUCT_SKU_RETAIL_PRICE * FACT_ORDER_LINE_ITEM_BASE.ORDERED_QUANTITY) AS "GROSS_SALES",  
+    FROM 
+        LU_PRD_PRODUCT_SKU
+    JOIN 
+        LU_PRD_PRODUCT
+    ON 
+        LU_PRD_PRODUCT_SKU.PRODUCT_SKU = LU_PRD_PRODUCT.PRODUCT_SKU
+    JOIN 
+        FACT_ORDER_LINE_ITEM_BASE 
+    ON 
+        FACT_ORDER_LINE_ITEM_BASE.PRODUCT_CODE = LU_PRD_PRODUCT.PRODUCT_CODE
+    JOIN 
+        LU_CUS_CUSTOMER 
+    WHERE
+        LU_PRD_PRODUCT_SKU.PRODUCT_SKU IN  ('{product_sku_trained_selected}')
+        AND (YEAR(FACT_ORDER_LINE_ITEM_BASE.ORDER_DATE) BETWEEN 2021 AND 2023)
+    GROUP BY
+        WEEK
+    ORDER BY
+        WEEK ASC
+""").collect()
+columns = ["WEEK", "GROSS_SALES"]
+weekly_gross_sales_by_product = convert_list_to_df(weekly_gross_sales_by_product, columns)
+
+#  ====== Data Exploration Part =========
+# plot the area chart
+st.area_chart(weekly_gross_sales_by_product, x="WEEK", y="GROSS_SALES", color=["#00CCDD"])
+# !will add more visual analysis here
+
+
+# ===== End of Data Exploration Part =====
+# find min and max volumn of select product
+if st.button("Train Model for this Product"):
+    # ============ Data collection Part ==============
+    st.write(f"Training model for product: {product_sku_trained_selected}") 
+    st.write(f"Data Collection")
+    raw_data = session.sql(f"""
+        SELECT 
+            FACT_ORDER_LINE_ITEM_BASE.ORDER_DATE AS "DATE",
+            LU_PRD_PRODUCT_SKU.PRODUCT_SKU_RETAIL_PRICE AS "RETAIL_RPICE",
+            REL_CUS_SEGMENTATION_RFM.CLUSTER AS "CUSTOMER_SEGMENT",
+            COUNT(DISTINCT(FACT_ORDER_LINE_ITEM_BASE.ORDER_CODE)) AS "NUM_ORDERS",
+            SUM(FACT_ORDER_LINE_ITEM_BASE.ORDERED_QUANTITY) AS "UNITS_SOLD",
+            AVG(FACT_ORDER_LINE_ITEM_BASE.DISCOUNT_RATE) AS "AVG_DISCOUNT_RATE",
+            SUM(FACT_ORDER_LINE_ITEM_BASE.SUPPLY_UNIT_COST) AS "COGS",
+        FROM 
+            LU_PRD_PRODUCT_SKU
+        JOIN 
+            LU_PRD_PRODUCT
+        ON 
+            LU_PRD_PRODUCT_SKU.PRODUCT_SKU = LU_PRD_PRODUCT.PRODUCT_SKU
+        JOIN 
+            FACT_ORDER_LINE_ITEM_BASE 
+        ON 
+            FACT_ORDER_LINE_ITEM_BASE.PRODUCT_CODE = LU_PRD_PRODUCT.PRODUCT_CODE
+        JOIN 
+            LU_CUS_CUSTOMER 
+        ON
+            LU_CUS_CUSTOMER.CUSTOMER_CODE = FACT_ORDER_LINE_ITEM_BASE.CUSTOMER_CODE
+        JOIN 
+            REL_CUS_SEGMENTATION_RFM
+        ON
+            REL_CUS_SEGMENTATION_RFM.CUSTOMER_CODE = FACT_ORDER_LINE_ITEM_BASE.CUSTOMER_CODE
+        WHERE
+            LU_PRD_PRODUCT_SKU.PRODUCT_SKU IN  ('{product_sku_trained_selected}')
+            AND (YEAR(FACT_ORDER_LINE_ITEM_BASE.ORDER_DATE) BETWEEN 2021 AND 2023)
+        GROUP BY
+            DATE, RETAIL_RPICE, CUSTOMER_SEGMENT
+        ORDER BY
+            DATE ASC
+    """).collect()
+
+    columns = ["DATE", "RETAIL_RPICE", "CUSTOMER_SEGMENT", "NUM_ORDERS", "UNITS_SOLD", "AVG_DISCOUNT_RATE", "COGS"]
+    df_raw_data = pd.DataFrame(raw_data, columns=columns)
+
+    # ============ Data preprocessing part ==============
+    st.write(f"Data Preprocessing")
+    # Change the data types of the columns
+    df_raw_data['UNITS_SOLD'] = df_raw_data['UNITS_SOLD'].astype(int)
+    df_raw_data['AVG_DISCOUNT_RATE'] = df_raw_data['AVG_DISCOUNT_RATE'].astype(float)
+    df_raw_data['COGS'] = df_raw_data['COGS'].astype(float)
+
+    # Outlier treatment
+    outlier_percentages = {}
+    numerical_columns = ['NUM_ORDERS', 'UNITS_SOLD', 'AVG_DISCOUNT_RATE', 'COGS']
+    for column in numerical_columns:
+        Q1 = df_raw_data[column].quantile(0.25)
+        Q3 = df_raw_data[column].quantile(0.75)
+        IQR = Q3 - Q1
+        lower_bound = Q1 - 1.5 * IQR
+        upper_bound = Q3 + 1.5 * IQR
+        outliers = df_raw_data[(df_raw_data[column] < lower_bound) | (df_raw_data[column] > upper_bound)]
+        outlier_percentage = (len(outliers) / len(df_raw_data)) * 100
+        outlier_percentages[column] = outlier_percentage
+    df_raw_data = treat_outliers(df_raw_data, 'NUM_ORDERS', outlier_percentages)
+    df_raw_data = treat_outliers(df_raw_data, 'UNITS_SOLD', outlier_percentages)
+    df_raw_data = treat_outliers(df_raw_data, 'AVG_DISCOUNT_RATE', outlier_percentages)
+    df_raw_data = treat_outliers(df_raw_data, 'COGS', outlier_percentages)
+
+    # Drop missing values
+    df_raw_data.dropna(inplace=True)
+    # Drop the duplicate rows
+    df_raw_data.drop_duplicates(inplace=True)
+
+    # Convert the DATE column to datetime
+    df_raw_data['DATE'] = pd.to_datetime(df_raw_data['DATE'])
+    df_raw_data['DATE_ORDINAL'] = df_raw_data['DATE'].apply(lambda x: x.toordinal())
+
+    retail_price = df_raw_data['RETAIL_RPICE'][0]
+    # ============== End of Data Preprocessing Part ==============
+    # ============== Feature Engineering Part ==============
+    st.write(f"Feature Engineering")
+    # feature selection
+    df_model = df_raw_data[['DATE_ORDINAL', 'UNITS_SOLD', 'CUSTOMER_SEGMENT', 'AVG_DISCOUNT_RATE']]
+    df_model['CUSTOMER_SEGMENT'] = df_model['CUSTOMER_SEGMENT'].astype('int')
+    # ============== End of Feature Engineering Part ==============
+
+    # =============== Data Splitting Part ==============
+    st.write(f"Data Splitting")
+    train_set, test_set = train_test_split(df_model, test_size=0.2, random_state=42)
+    df_model['PRODUCT_SKU'] = product_sku_trained_selected
+    # =============== End of Data Splitting Part ==============
+
+    # ============== Store preprocessed data to snowflake ==============
+    st.write(f"Insert Clean Data to snowflake!")
+    training_table = "FACT_DYNAMIC_PRICING_CLEANED"
+    # schema structure for pandas DataFrame convert to Snowpark DataFrame
+    schema = StructType([
+            StructField("DATE_ORDINAL", IntegerType()),
+            StructField("UNITS_SOLD", IntegerType()),
+            StructField("CUSTOMER_SEGMENT", IntegerType()),
+            StructField("AVG_DISCOUNT_RATE", FloatType()),
+            StructField("PRODUCT_SKU", StringType())
+        ])
+    create_table_if_not_exists(training_table)
+    snowpark_df = insert_pandas_dataframe_to_snowflake(session, df_model, training_table, schema)
+    # ============== End of storing preprocessed data to snowflake ==============
+
+    # =============== Create a stage for the model ==============
+    st.write(f"Stage Creation if not exists")
+    stage_name = "DYNAMIC_PRICING_MODEL_STAGE"
+    session.sql(f"CREATE STAGE IF NOT EXISTS {stage_name}").collect()
+
+    # =============== Model Training Part ==============
+    st.write(f"Model Training start.........")
+    training_table = "FACT_DYNAMIC_PRICING_CLEANED"
+    model_file_name = f"{product_sku_trained_selected}_xgb_model.sav"
+    feature_cols = ['DATE_ORDINAL','UNITS_SOLD','CUSTOMER_SEGMENT']
+    target_col = 'AVG_DISCOUNT_RATE'
+    params = {
+        'n_estimators': [100, 200],
+        'max_depth': [5, 7],
+        'learning_rate': [0.05, 0.01],
+        'subsample': [0.8, 1.0],
+        'colsample_bytree': [0.8, 1.0]
+    }
+    # calling the stored procedure to train and save the model
+    result = session.call(
+        "sproc_train_xgb_model1",
+        training_table,
+        feature_cols,
+        target_col,
+        model_file_name,
+        stage_name,
+        params
+    )
+    # =============== Model Evaluation Part ==============
+    st.write(f"Model Evaluation")
+    st.write(result)
+    # ----- Write code where all models are compared and best model is selected ----
+
+    # =============== End of Model Evaluation Part ==============
+    model_files = get_modile_file_name_from_stage(session, stage_name)
+
+    # re-defined udf_avg_discount_rate_prediction with new import model list
+    session.udf.register(
+        func=avg_discount_rate_prediction,
+        name="udf_avg_discount_rate_prediction",
+        stage_location=stage_name,
+        input_type=[T.IntegerType(), T.IntegerType(), T.IntegerType(), T.StringType()],  # Three integers and one string
+        return_type=T.FloatType(),  # The return type is a float
+        replace=True,
+        is_permanent=True,
+        # imports=[f"@{stage_name}/{model_file_name}"],  # Model file is imported,
+        imports=[f"@{stage_name}/{model}" for model in model_files], # multiple model files are imported, 
+        packages=["joblib", "cachetools", "xgboost","pandas"],  # Required packages for the UDF
+    )
+
+    # =============== End of Model Training Part ==============
+    
+    # =============== Store result to snowflake =================
+    st.write(f"Store result to snowflake!")
+
+    parsed_result = json.loads(result)
+    evaluation_metrics = {
+        "mse": parsed_result['mse']
+    }
+    hyperparameters = {
+        "best_params": parsed_result['best_params']
+    }
+    # push trained product to snowflake
+
+    insert_trained_result_to_snowflake(session, product_sku_trained_selected, df_model, model_file_name, evaluation_metrics, hyperparameters)
+    # =============== End of storing result to snowflake ==============
+    st.write("Model training process completed successfully")                         
---
+++ product_price_recommendation.py
@@ -0,0 +1,242 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import os
+# import snowflake.connector as sf # type: ignore
+
+# from dotenv import load_dotenv # type: ignore
+# from joblib import load
+from datetime import datetime, timedelta
+
+#import snowflake model
+from snowflake.snowpark import Session
+import snowflake.snowpark.functions as F
+
+from dotenv import load_dotenv
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from xgboost import XGBRegressor # type: ignore
+from joblib import dump
+# from snowflake.snowpark.context import get_active_session
+
+st.set_page_config(page_title="Price Recommendation", page_icon="🏷️")
+
+@st.cache_resource(ttl=3600)
+def init_connection():
+    #get account credentials from
+    load_dotenv()
+    connection_parameters = {
+        "user": os.getenv("SNOWFLAKE_USER"),
+        "password": os.getenv("SNOWFLAKE_PASSWORD"),
+        "account": os.getenv("SNOWFLAKE_ACCOUNT"),
+        "warehouse": os.getenv("SNOWFLAKE_WAREHOUSE"),
+        "database": os.getenv("SNOWFLAKE_DATABASE"),
+        "schema": os.getenv("SNOWFLAKE_SCHEMA"),
+        "role": os.getenv("SNOWFLAKE_ROLE"),
+        "query_tag": "streamlit-app product-training"
+    }
+    return Session.builder.configs(connection_parameters).create()
+
+def convert_list_to_df(data, columns):
+    return pd.DataFrame(data, columns=columns)
+
+# ============ End of function Definitions =========
+# ============ Main Code ==============
+
+session = init_connection()
+# session = get_active_session()
+
+st.title('Pricing Recommendation App')
+
+# read list of products from snowflake LU_PRD_PRODUCT_TRAINED
+product_list = session.sql("""
+    SELECT  
+        PRODUCT_SKU
+    FROM 
+        LU_PRD_PRODUCT_TRAINED
+    ORDER BY 
+        DATE_MODIFIED DESC
+""").collect()
+
+product = [product[0] for product in product_list]
+#product = ['B33_S3','B15_S1', 'B13_S1', 'B31_S32', 'B44_S4', 'B15_S47']
+#print(product)
+
+product = st.selectbox(
+    'Choose a Product',
+    product
+)
+selected_product_retail_price = session.sql(f"""
+    SELECT 
+        LU_PRD_PRODUCT_SKU.PRODUCT_SKU_RETAIL_PRICE 
+    FROM LU_PRD_PRODUCT_TRAINED 
+        JOIN LU_PRD_PRODUCT_SKU 
+    ON 
+        LU_PRD_PRODUCT_SKU.PRODUCT_SKU = LU_PRD_PRODUCT_TRAINED.PRODUCT_SKU 
+    WHERE 
+        LU_PRD_PRODUCT_TRAINED.PRODUCT_SKU = '{product}'
+""").collect()
+#print(type(selected_product_retail_price))
+#print(selected_product_retail_price)
+product_retail_price_display = selected_product_retail_price[0]['PRODUCT_SKU_RETAIL_PRICE']
+# print(product_retail_price)
+st.caption(f"{product} base price: \\${product_retail_price_display:.2f}")
+
+# find min and max volumn of select product
+min_max_volume_retail_price_latest_sales = session.sql(f"""
+    SELECT 
+        LU_PRD_PRODUCT_TRAINED.MIN_VOLUMN_SOLD AS "MIN_VOLUMN_SOLD", 
+        LU_PRD_PRODUCT_TRAINED.MAX_VOLUMN_SOLD AS "MAX_VOLUMN_SOLD",
+        LU_PRD_PRODUCT_SKU.PRODUCT_SKU_RETAIL_PRICE "RETAIL_PRICE",
+        LU_PRD_PRODUCT_SKU.LATEST_DATE AS "LATEST_DATE_SOLD"
+    FROM 
+        LU_PRD_PRODUCT_TRAINED 
+    JOIN 
+        LU_PRD_PRODUCT_SKU 
+    ON 
+        LU_PRD_PRODUCT_SKU.PRODUCT_SKU = LU_PRD_PRODUCT_TRAINED.PRODUCT_SKU 
+    WHERE 
+        LU_PRD_PRODUCT_TRAINED.PRODUCT_SKU = '{product}'
+""").collect()
+
+lastest_sold = min_max_volume_retail_price_latest_sales[0]['LATEST_DATE_SOLD'] + timedelta(days=1)
+after_lastest_sold = min_max_volume_retail_price_latest_sales[0]['LATEST_DATE_SOLD'] + timedelta(days=7)
+
+start_date, end_date = st.date_input(
+    "Select Date Range for Forecast",
+    value = [lastest_sold, after_lastest_sold],  # Default range: today to one week from today
+    min_value = lastest_sold  # Setting the minimum selectable date as today
+)
+
+num_days_selected = (end_date - start_date).days + 1
+# print(f"Number of days selected: {num_days_selected}")
+min_volume = max(min_max_volume_retail_price_latest_sales[0]['MIN_VOLUMN_SOLD'], num_days_selected)  # Ensure minimum volume is at least number of days
+max_volume = min_max_volume_retail_price_latest_sales[0]['MAX_VOLUMN_SOLD']
+retail_price = min_max_volume_retail_price_latest_sales[0]['RETAIL_PRICE']
+
+volume = st.number_input(
+    f"Enter the Expected Volume Sold (Recommended Range: {min_volume} - {max_volume*num_days_selected})",  # Label for the input
+    min_value=min_volume,  # Minimum value based on the product
+    value=min_volume*100,  # Default value
+    step=1 # Step for incrementing/decrementing
+)
+
+# ======Distribute the volume across the selected days=======
+volume_per_day = volume // num_days_selected  # Base volume per day
+remaining_volume = volume % num_days_selected  # Remaining volume to distribute
+# Create a list to store the volume distribution
+volume_distribution = [volume_per_day] * num_days_selected
+
+# Add bias to the first `remaining_volume` days
+for i in range(remaining_volume):
+    volume_distribution[i] += 1  # Distribute the extra volume to the earlier days
+
+list_of_dates = [start_date + timedelta(days=i) for i in range(num_days_selected)]
+# Three clusters of customers (But there will be more)
+cluster_members = [0,1,2]
+# Create a list of cluster samples for each day
+cluster_samples = [[clusters]*num_days_selected for clusters in cluster_members]
+
+#===Debugging===
+# print("Volumn Distribution")
+# print(volume_distribution)
+# print("Cluster Samples")
+# print(cluster_samples)
+
+# print(f"Volume Distribution: {volume_distribution}")
+# Button to Show Images and Prices
+if st.button('Pricing Recommendations'):
+    st.markdown('---')
+    st.subheader('Product Pricing Recommended Range')
+
+    
+
+    each_cluster_average_discount_rate = []
+    for cluster_number in cluster_members:
+        sample_data = {
+            'UNIT_SOLD': volume_distribution,
+            'DATE': list_of_dates,
+            #'CLUSTER': [1,1,1,1,1,1,1,1],
+            'CLUSTER': cluster_samples[cluster_number],
+        }
+
+        df_sample = pd.DataFrame(sample_data)
+
+        # ================= Data Preprocessing Steps =====================
+        df_sample['DATE'] = pd.to_datetime(df_sample['DATE'])
+        df_sample['DATE_ORDINAL'] = df_sample['DATE'].apply(lambda x: x.toordinal())
+        # Drop the original DATE column
+        df_sample = df_sample.drop(columns=['DATE'])
+
+        # Predict using the trained model
+        # Reorder the columns of df_sample
+        df_sample = df_sample[['DATE_ORDINAL', 'UNIT_SOLD', 'CLUSTER']]
+        # print(df_sample)
+        
+        snowpark_df_sample = session.create_dataframe(df_sample)
+
+        # ================= Predict the Average Discount Rate from UDFs snowflake===================
+        predictions_set = snowpark_df_sample.select(
+            F.col("DATE_ORDINAL"),
+            F.col("UNIT_SOLD"),
+            F.col("CLUSTER"),
+            F.call_udf("udf_avg_discount_rate_prediction", 
+                snowpark_df_sample['DATE_ORDINAL'],
+                snowpark_df_sample['UNIT_SOLD'], 
+                snowpark_df_sample['CLUSTER'], 
+                "B33_S3_xgb_model.sav").alias('PREDICTED_AVG_DISCOUNT_RATE')
+        ).collect()
+        # print("predictions_set")
+        # print(predictions_set)
+        # Extract the 'PREDICTED_AVG_DISCOUNT_RATE' column from each row in predictions_set
+        predicted_discount_rates = [row['PREDICTED_AVG_DISCOUNT_RATE'] for row in predictions_set]
+
+        # Calculate the minimum value from the 'PREDICTED_AVG_DISCOUNT_RATE' column
+        min_discount_rate = min(predicted_discount_rates)
+        max_discount_rate = max(predicted_discount_rates)
+
+        # print(f"Cluster {cluster_number} - Min Discount Rate: {min_discount_rate}, Max Discount Rate: {max_discount_rate}")
+        # Store min and max discount rate for each cluster
+        each_cluster_average_discount_rate.append({
+            'min_discount_rate': min_discount_rate,
+            'max_discount_rate': max_discount_rate
+        })
+
+    print("Each Cluster Average Discount Rate")  
+    print(each_cluster_average_discount_rate)
+
+    min_price_cs1 = retail_price * (1 - each_cluster_average_discount_rate[0]['min_discount_rate'])
+    max_price_cs1 = retail_price * (1 - each_cluster_average_discount_rate[0]['max_discount_rate'])
+    min_price_cs2 = retail_price * (1 - each_cluster_average_discount_rate[1]['min_discount_rate'])
+    max_price_cs2 = retail_price * (1 - each_cluster_average_discount_rate[1]['max_discount_rate'])
+    min_price_cs3 = retail_price * (1 - each_cluster_average_discount_rate[2]['min_discount_rate'])
+    max_price_cs3 = retail_price * (1 - each_cluster_average_discount_rate[2]['max_discount_rate'])
+
+    col1, col2, col3 = st.columns(3)
+
+    
+    with col1:
+        st.image('CS 1.png', caption='Customer Segment 1') 
+        st.write(f"Price Range: \\${min_price_cs1:.2f} - \\${max_price_cs1:.2f}")
+
+    
+    with col2:
+        st.image('CS 2.png', caption='Customer Segment 2')
+        st.write(f"Price Range: \\${min_price_cs2:.2f} - \\${max_price_cs2:.2f}")
+
+    
+    with col3:
+        st.image('CS 3.png', caption='Product Image 3')
+        st.write(f"Price Range: \\${min_price_cs3:.2f} - \\${max_price_cs3:.2f}")
+
+    st.markdown('---')
+    st.subheader('Additional Information Range')
+
+    
+    st.write(f"Selected Product: {product}")
+    st.write(f"Product Base Price: \\${retail_price}")
+    st.write(f"Expected Volume Sold: {volume} units")
+    st.write(f"Start Date: {start_date.strftime('%Y-%m-%d')}")
+    st.write(f"End Date: {end_date.strftime('%Y-%m-%d')}")
+
+
